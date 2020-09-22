d97676f681b1e2bd8de5fafab3e2a374635971d4
Charles Walker <walkercharlesmw99@gmail.com>
2020-09-22 14:16:44 -0700

    initial commit

--- Pa2/main.swift
+++ Pa2/main.swift
@@ -9,4 +9,25 @@
 import Foundation
 
 print("Hello, World!")
+//1. familiarize yourelf with the problem
+//2. look for nouns
+//3. identify the verbs in the problem
+//  how do the nouns interact
+//4. model nouns as types and model verbs as methods
+// start with the fundamnetal unit of thr problem
+struct Card: CustomStringConvertible {
+    var name: String
+    var color: String
+    var description: String {
+        return "\(color)\(name)\(color)"
+    }
+    
+    var secretDescription: String {
+        return "card"
+    }
+}
 
+//can i make a card that models a blue 2
+let card = Card(name: "2", color: "blue")
+print("how i see it:", card)
+print("how they see it:", card.secretDescription)
