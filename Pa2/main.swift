0cda58b2f3acfdb3af186f12af8f87feb1833ba7
Charles Walker <walkercharlesmw99@gmail.com>
2020-09-24 12:21:40 -0700

    new

--- Pa2/main.swift
+++ Pa2/main.swift
@@ -15,6 +15,8 @@ print("Hello, World!")
 //  how do the nouns interact
 //4. model nouns as types and model verbs as methods
 // start with the fundamnetal unit of thr problem
+
+//
 struct Card: CustomStringConvertible {
     var name: String
     var color: String
@@ -28,6 +30,51 @@ struct Card: CustomStringConvertible {
 }
 
 //can i make a card that models a blue 2
+
+struct Hand: CustomStringConvertible {
+    var Cards: [Card] = []
+    var numCards: Int
+    var description: String {
+        return "\(Cards)"
+    }
+    var secretDescript: String {
+        let secretCards = [String] (repeating: "card", count: Cards.count)
+        return "\(secretCards)"
+    }
+}
+
+struct Player: CustomStringConvertible {
+    var num: Int
+    var hand: Hand
+    var description: String {
+        return "Player\(num) : \(hand)"
+    }
+}
+
+struct Deck: CustomStringConvertible {
+    var drawPile: [Card] = []
+    var discardPile: [Card] = []
+    var description: String {
+        let topCard = discardPile.last
+        return "\(topCard)"
+    }
+}
+
+func buildDeck () -> [Card] {
+    
+}
+class UnoGame {
+    
+}
+
 let card = Card(name: "2", color: "blue")
+let card2 =  Card(name: "3", color: "green")
+let hand = Hand(Cards: [card, card2], numCards: 2)
+let player1 = Player(num: 1, hand: hand)
+let deck = Deck(drawPile: [card,card2], discardPile: [card,card2])
 print("how i see it:", card)
 print("how they see it:", card.secretDescription)
+//print(hand)
+//print(hand.secretDescript)
+print(player1)
+print(deck)
