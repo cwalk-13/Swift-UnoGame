aa55bea563540f9c35a3254879628ab5e88d1885
Charles Walker <walkercharlesmw99@gmail.com>
2020-09-24 14:44:02 -0700

    Initial Commit

---
+++ pa2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  pa2
+//
+//  Created by Walker, Charles Milton on 9/24/20.
+//  Copyright © 2020 Walker, Charles Milton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
